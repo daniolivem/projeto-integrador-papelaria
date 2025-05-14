0f90fb30a3b146aafbbf7c0d81d3a0e567b996fe
Carlos <carlosotacilio65@gmail.com>
2025-05-14 20:41:11 -0300

    adicionei notificação de itens adicionados ao carrinho, no ícone do carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -5,6 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
       localStorage.setItem("carrinho", JSON.stringify(carrinho));
    }
 
+   //Contador de Itens no  Carrinho
+   function atualizarContadorCarrinho() {
+      const contador = document.getElementById("contador-carrinho");
+      contador.textContent = carrinho.length;
+
+      if (carrinho.length > 0) {
+         contador.style.display = "flex";
+      } else {
+         contador.style.display = "none";
+      }
+   }
+
    function renderizarCarrinho() {
       const container = document.getElementById("itensCarrinho");
       const totalCompra = document.getElementById("totalCompra");
@@ -22,9 +34,14 @@ document.addEventListener("DOMContentLoaded", function () {
             <button onclick="removerItem(${index})">X</button>
          `;
          container.appendChild(itemDiv);
+
+         
       });
 
       totalCompra.textContent = `Total: R$${total.toFixed(2)}`;
+
+      // Atualiza o contador de itens
+      atualizarContadorCarrinho();
    }
 
    window.adicionarAoCarrinho = function (nome, preco) {
